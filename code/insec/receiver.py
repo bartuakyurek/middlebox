2f5c7a083077d686e2b018aa4433cf3aa9ef2dd7
Bartu <77360680+bartuakyurek@users.noreply.github.com>
2025-04-12 19:46:48 +0300

    Add utility to extract covert bits
    - The receiver had a function to convert a message to a bitstream, here we receive a bitstream and convert it to message back.
    - Refactor the loop I was thinking in the previous commit, now the try/excepts are only in main, and listener itself has the loop.

--- code/insec/receiver.py
+++ code/insec/receiver.py
@@ -4,12 +4,22 @@ from scapy.all import IP, UDP, Raw, send
 
 # ------------------------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------------------------
+def assert_type(obj, desired_type, note=""):
+    assert isinstance(obj, desired_type), f"[ERROR] Expected {note} to be type {desired_type}, got {type(obj)}"
+
+def bits_to_message(bits: str)->str:
+    assert_type(bits, str, "bits")
+    chars = [bits[i:i+8] for i in range(0, len(bits), 8)]
+    return ''.join([chr(int(c, 2)) for c in chars])
+
 
 class CovertReceiver:
 
     def __init__(self, port=8888):
         self.port = port
         self.sock = self.create_and_bind_socket(port)
+
+        self.covert_bits = {} # Store in a dictionary because number of bits is unknown
 
     def create_and_bind_socket(self, port):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -19,7 +29,17 @@ class CovertReceiver:
         print(f"UDP listener started on port {port}")
         return sock
 
-    def _udp_loop(self):
+    def shutdown(self):
+        self.sock.close()
+        print("[INFO] Socket closed.")
+
+    def get_covert_msg(self):
+        if self.bits:
+            bit_str = ''.join(self.bits[i] for i in sorted(self.bits))
+            return bits_to_message(bit_str)
+        return ""
+    
+    def start_udp_listener(self):
         while True:
                 data, addr = self.sock.recvfrom(4096)
                 print(f"Received {len(data)} bytes from {addr}")
@@ -31,18 +51,9 @@ class CovertReceiver:
                     sent = self.sock.sendto(data, addr)
                     print(f"Sent {sent} bytes (ACK) back to {addr}")
 
-    def start_udp_listener(self):
-        try:
-            self._udp_loop()
-        except Exception as e:
-            print(f"An error occurred: {e}")
-        finally:
-            self.sock.close()
-            print("[INFO] Socket closed.")
-
+    
 # ------------------------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------------------------
-
 
 if __name__ == "__main__":
     
@@ -52,3 +63,8 @@ if __name__ == "__main__":
         receiver.start_udp_listener()
     except KeyboardInterrupt:
         print("Receiver stopped.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        receiver.shutdown()
+        print("\nCovert message:", receiver.get_covert_msg())
