57735983c84b2440824717fbe3dab1137f5a2c57
Bartu <77360680+bartuakyurek@users.noreply.github.com>
2025-04-12 12:14:45 +0300

    Implement CovertSender.send( )
    Previously a whole chunk of message was sent through this class, now we can call .send() to send messages separately, it still breaks the message into chunks to fit into UDP datagram.
    
    At this point it just waits until a single packet delivered successfully, a multithreaded approach would be great to send a batch of packets and implement sliding window to allow reliability.

--- code/sec/sender.py
+++ code/sec/sender.py
@@ -23,24 +23,39 @@ def assign_sequence_number(msg_str, seq_number)->str:
     return msg_with_sequence
 
 class CovertSender:
-    def __init__(self, overt_msg, covert_msg, start_immediately=False, verbose=False, timeout=5):
-        self.host_ip = self.get_host()
+    def __init__(self, covert_msg, verbose=False, timeout=5):        
         self.verbose = verbose
         self.timeout = timeout
+        self.covert_msg = covert_msg
+
+        self.host_ip = self.get_host()
+        self.port = 8888
+        self.sock = self.create_socket()
         
-        if start_immediately: self.start_udp_sender(overt_msg)
-        elif verbose: print("[INFO] CovertSender created. Call start_udp_sender() to start sending packets.")
+        if verbose: print("[INFO] CovertSender created. Call send() to start sending packets.")
 
     def get_host(self, IP_NAME='INSECURENET_HOST_IP'):
         host = os.getenv(IP_NAME)
         if not host:
             raise ValueError("SECURENET_HOST_IP environment variable is not set.")
         return host
+    
+    def create_socket(self, host_ip=None):
+         if host_ip is None: host_ip = self.host_ip
+         else: 
+             self.host_ip = host_ip
+             print(f"[WARNING] Host IP changed to {host_ip}")
 
-    def _udp_with_ACK(self, sock, message, host, port, timeout, max_resend=10)->int:
+         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP socket
+         return sock
+    
+    def shutdown(self):
+        self.sock.close()
+
+    def _udp_with_ACK(self, message, max_resend=10)->int:
         # Returns 0 if message sent successfully
         # -1 if it cannot be delivered in max_resend trials.
-        sock.settimeout(timeout)
+        self.sock.settimeout(self.timeout)
         trials = 0
         while True:
             trials += 1
@@ -50,61 +65,57 @@ class CovertSender:
                 encoded_msg = message.encode()
                 assert len(encoded_msg) < MAX_UDP_PAYLOAD_SIZE, f"Maximum UDP payload is exceeded ({len(encoded_msg)}>{MAX_UDP_PAYLOAD_SIZE}), the message should be splitted into chunks."
 
-                sock.sendto(encoded_msg, (host, port))
-                if self.verbose: print(f"Message sent to {host}:{port}")
+                self.sock.sendto(encoded_msg, (self.host_ip, self.port))
+                if self.verbose: print(f"[INFO] Message sent to {self.host_ip}:{self.port}")
 
                 # Wait for acknowledgment
-                response, server = sock.recvfrom(4096)
+                response, server = self.sock.recvfrom(4096)
                 if response.decode() == "ACK":
-                    if self.verbose: print("Acknowledgment received. Message delivered successfully.")
+                    if self.verbose: print("[INFO] Acknowledgment received. Message delivered successfully.")
                     break
-            except socket.timeout:
-                print("Timeout occurred. Resending message...")
+            except socket.self.timeout:
+                print("[INFO] Timeout occurred. Resending message...")
                 
         return 0
             
-    def start_udp_sender(self, message, host_ip=None, port=8888):
-        if host_ip is None: host_ip = self.host_ip
-        try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP socket
-            if self.verbose: print("[INFO] Socket created successfully.")
+    def send(self, message):
+        # Sends a legitimate message
+        # If the given message cannot fit into a single packet
+        # it splits up and sends multiple packets
+        # TODO: Send covert bits alongside
+            
+        if self.verbose: print("[INFO] Socket created successfully.")
 
-            encoded_msg = message.encode() 
-            encoded_msg_chunks = split_message_into_chunks(encoded_msg, MAX_UDP_PAYLOAD_SIZE-8) # -8 is to be able to add sequence number in the beginning 
-            if self.verbose: print(f"[INFO] Message is splitted into {len(encoded_msg_chunks)} chunks.")
+        encoded_msg = message.encode() 
+        encoded_msg_chunks = split_message_into_chunks(encoded_msg, MAX_UDP_PAYLOAD_SIZE-8) # -8 is to be able to add sequence number in the beginning 
+        if self.verbose: print(f"[INFO] Message is splitted into {len(encoded_msg_chunks)} chunks.")
 
-            # Send message packets
-            for i, msg in enumerate(encoded_msg_chunks):
-                msg_str = assign_sequence_number(msg.decode(), i)
-                if self.verbose: print(f"[INFO] Appended sequence number to message: {msg_str}")
+        # Send message packets
+        for i, msg in enumerate(encoded_msg_chunks):
+            msg_str = assign_sequence_number(msg.decode(), i)
+            if self.verbose: print(f"[INFO] Appended sequence number to message: {msg_str}")
 
-                while True: # Send a single packet until success (TODO: use threaded)
-                    udp_status = self._udp_with_ACK(sock, msg_str, host_ip, port, self.timeout)
-                    if udp_status == 0: break # Move on to next message
+            while True: # Send a single packet until success (TODO: use threaded)
+                udp_status = self._udp_with_ACK(msg_str)
+                if udp_status == 0: break # Move on to next message
 
-        except Exception as e:
-            print(f"[ERROR] An error occurred: {e}")
-        finally:
-            sock.close()
-
+        
 # ------------------------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     # TODO: Add timeout parameter, overt msg, covert msg
-
-    overt_msg = "hello, world! this is an overt message. " * 100
-    covert_msg = "this is a covert message."
-
-    sender = CovertSender(overt_msg=overt_msg, 
-                          covert_msg=covert_msg, 
-                          start_immediately=True,
-                          verbose=True)
     
-    #TODO:
-    #overt_messages = ["hello " for _ in range(10)]
-    #covert_msg = "this is covert."
-    #sender = CovertSender(covert_msg=covert_msg, verbose=True)
-    #for msg in overt_messages:
-    #    sender.send(msg)
+    overt_messages = ["hello " for _ in range(10)]
+    covert_msg = "this is covert."
+    sender = CovertSender(covert_msg=covert_msg, verbose=True)
+
+    # Send messages
+    try:
+        for msg in overt_messages:
+            sender.send(msg) 
+    except Exception as e:
+            print(f"[ERROR] An error occurred: {e}")
+    finally:
+            sender.shutdown()
