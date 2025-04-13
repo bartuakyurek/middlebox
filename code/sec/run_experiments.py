47c905914cda45b1a5186a57476af507cf86a662
Bartu <77360680+bartuakyurek@users.noreply.github.com>
2025-04-13 13:56:15 +0300

    Get tests for windowsize
    Now it's able to test for different window sizes, though it's only tested
    for small messages, final tests will take longer.

--- code/sec/run_experiments.py
+++ code/sec/run_experiments.py
@@ -14,9 +14,11 @@ on its mechanism to ensure the packets are sent correctly.
 
 from sender import run_sender, get_args
 
-# Parameters to test:
+# Test for a small message for now
+CARRIER_MESSAGE = "Hello, this is a test message. " * 100
+COVERT_MESSAGE = "COWOW"
 
-
+# Parameters to test
 window_sizes = [1, 2, 4, 8]
 timeout_values = [0.01, 0.1, 0.2, 0.5, 1.0]
 max_allowed_retransmissions = [1, 2, 3, 4, 5] # TODO: 1 means do not retransmit, but it's confusing with this name, make the naming consistent
@@ -27,23 +29,35 @@ def run_capacity_test(sender):
     return
 
 
-def run_and_retrieve_statistics(args):
+def run_and_retrieve_statistics(args)-> dict:
     # Run sender fully then retrieve statistics
+
+    stats = {}
     sender = run_sender(args) 
-    print(">> Sender capacity: ", sender.get_capacity())
-    print("TODO: Add more statistics here...")
+
+    stats['capacity'] = sender.get_capacity()
+    print(">> Sender capacity: ", stats['capacity'])
+    
+    #rint("TODO: Add more statistics here...")
+    return stats
 
 def run_experiments(args):
     
     default_args = args
+
+    args.overt = CARRIER_MESSAGE # Override them to test for small messages
+    args.covert = COVERT_MESSAGE
     # Choose the parameters to test
     # Set the args with the chosen parameters
+    w_stats = {}
     for w_size in window_sizes:
         args.window_size = w_size
-        run_and_retrieve_statistics(args)
+        stat = run_and_retrieve_statistics(args)
+        w_stats[w_size] = stat
 
     print("Default window was: ", default_args.window_size) 
     print("Current args window size: ", args.window_size)
+    print("Window size capacity statistics: ", w_stats)
 
 if __name__ == "__main__":
     
