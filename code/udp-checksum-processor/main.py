312af92a270462f1754d089d6f1f4252b6732662
Bartu <77360680+bartuakyurek@users.noreply.github.com>
2025-05-29 10:01:19 +0300

    Implement mitigation strategy
    The simplest strategy is to enforce checksums, here receiver do not
    get anything because even preamble is not detected, but sender thinks
    it's received

--- code/udp-checksum-processor/main.py
+++ code/udp-checksum-processor/main.py
@@ -3,14 +3,15 @@ import random
 import asyncio
 import argparse 
 
-from scapy.all import Ether
+from scapy.all import Ether, IP, UDP, Raw
 from nats.aio.client import Client as NATS
 
 class UDP_Checksum_Processor:
-    def __init__(self, nc, topic_dict, mean_delay=1e-2):
+    def __init__(self, nc, topic_dict, mean_delay=1e-2, mitigate=False):
         self.nc = nc
         self.topic_dict = topic_dict
         self.mean_delay = mean_delay
+        self.mitigate_bool = mitigate
 
     async def subscribe(self):
         # Subscribe to inpktsec and inpktinsec topics
@@ -28,21 +29,45 @@ class UDP_Checksum_Processor:
         # This gives guarantee that the server has processed above message.
         await self.nc.flush(timeout=1)
        
+    async def mitigate(self, packet):
+        # Mitigation strategy: Enforce checksum 
+        # (i.e. always correct it, another strategy would be to always drop it, or corrupt)
+    
+        if UDP in packet:
+            ip_layer = packet[IP]
+            udp_layer = packet[UDP]
+            original_checksum = udp_layer.chksum
+
+            # Recompute checksum 
+            udp_layer.chksum = None
+            rebuilt_packet = Ether(bytes(packet))  
+            new_checksum = rebuilt_packet[UDP].chksum
+            print(f"[DEBUG] Original checksum: {original_checksum}")
+            print(f"[DEBUG] Recomputed checksum: {new_checksum}")
+
+            udp_layer.chksum = new_checksum
+            packet = Ether(bytes(packet))
+        return packet
+
     async def message_handler(self, msg):
         subject = msg.subject
         data = msg.data 
         
         packet = Ether(data)
+        print("[DEBUG] Original Packet:")
         print(packet.show())
 
-        #delay = random.expovariate(1. / self.mean_delay)
+        if self.mitigate_bool:
+            modified_packet = await self.mitigate(packet)
+        else:
+            modified_packet = packet
+
         delay = random.uniform(0, self.mean_delay * 2)
         await asyncio.sleep(delay)
-        
-        await self.publish(subject, msg.data)
+        await self.publish(subject, bytes(modified_packet)) 
 
 
-async def run(mean_delay=0):
+async def run(mean_delay=0, mitigate=False):
     nc = NATS()
 
     nats_url = os.getenv("NATS_SURVEYOR_SERVERS", "nats://nats:4222")
@@ -53,7 +78,7 @@ async def run(mean_delay=0):
                     "inpktinsec" : "outpktsec"
     }
 
-    processor = UDP_Checksum_Processor(nc, topic_dict, mean_delay)
+    processor = UDP_Checksum_Processor(nc, topic_dict, mean_delay, mitigate)
     await processor.subscribe()
 
     try:
@@ -69,10 +94,11 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-d', '--delay', type=float, default=1e-2, help='Specify the average delay to be added before sending packets in seconds.')
+    parser.add_argument('-m', '--mitigate', help='Run covert channel mitigation strategy. Default False.', action="store_true", default=False)
 
     args = parser.parse_args()
     
     print("Running processor with delay ", args.delay)
-    asyncio.run(run(args.delay))
+    asyncio.run(run(args.delay, args.mitigate))
 
  
